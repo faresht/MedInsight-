8a2278393a15ca6d8ee6272ff7a8fc422c1a76fa
faresht <fares.htiouech@sesame.com.tn>
2026-02-04 21:58:45 +0100

    Fix doctor-service tests: Exclude default filters in ComponentScan

--- doctor-service/src/main/java/com/medinsight/doctor/DoctorServiceApplication.java
+++ doctor-service/src/main/java/com/medinsight/doctor/DoctorServiceApplication.java
@@ -3,9 +3,15 @@ package com.medinsight.doctor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.medinsight.doctor", "com.medinsight.common" })
+@ComponentScan(basePackages = { "com.medinsight.doctor", "com.medinsight.common" }, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+        @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class)
+})
 public class DoctorServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DoctorServiceApplication.class, args);
